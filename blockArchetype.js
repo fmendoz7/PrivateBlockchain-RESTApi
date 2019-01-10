9ec469b06ef6922f68fe9334f2215463f7a7fa8a
Francis Mendoza <fmendoz7@asu.edu>
2019-01-10 21:55:50 +0000

    Pasted blockArchetype.js

---
+++ blockArchetype.js
@@ -0,0 +1,13 @@
+class BlockArchetype
+{
+    constructor(data)
+    {
+        this.hash = '',
+        this.height = 0,
+        this.body = data,
+        this.time = 0,
+        this.previousBlockHash = ''
+    }
+}
+
+module.exports = BlockArchetype;
